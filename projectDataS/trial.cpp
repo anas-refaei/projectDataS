3218d3d4a36eeefa1e6914a413779cac5408f9b0
anas-refaei <s-anas.refaei@zewailcity.edu.eg>
2025-11-18 20:22:16 +0200

    Add Add trial.cpp source code trial.cpp source code

---
+++ projectDataS/trial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "====================================" << endl;
+    cout << "Restaurant Management System" << endl;
+    cout << "CIE 205 - Data Structures Project" << endl;
+    cout << "====================================" << endl;
+    cout << endl;
+    cout << "Project initialized successfully!" << endl;
+    cout << "Team members ready to collaborate!" << endl;
+    return 0;
+}
+
