b65f8c7f82337e970db712c0a8f5c51e9105cade
anas-refaei <s-anas.refaei@zewailcity.edu.eg>
2025-11-20 15:47:14 +0200

    newwww

--- projectDataS/PriorityQueue.h
+++ projectDataS/PriorityQueue.h
@@ -1,1 +1,40 @@
-#pragma once
+#include <iostream>
+using namespace std;
+//?????? ??? ?? ?????   
+// We can't use the Queue.h instead of this priorityQueue.h as there are diffrent structure Like (float priority.)
+
+
+template <typename T>
+class PriorityQueue {
+private:
+	struct PriorityNode {
+		T item;
+		float priority;
+		PriorityNode* next;
+		PriorityNode() {                       //CONSTRUCTOR
+			item = 0;
+			priority = 0.0;
+			next= nullptr
+
+		}
+		PriorityNode(T data, float p) {                  //CONSTRUCTOR
+			this->item = data;
+			this->priority = p;
+			this->next = nullptr;
+		}
+
+	};
+	PriorityNode* head=nullptr;  
+	int count=0;
+public:                           //CONSTRUCTOR
+	PriorityQueue() {
+
+		head = nullptr;
+		count=0;
+	}
+	~PriorityQueue() {
+		clear();                  // Delete all nodes (DESRUCTOR)
+	}
+
+
+};
